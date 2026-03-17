b2051ec5ae42e3230113255cf27f1f29d27c69cd
lmm <lumengmengs@163.com>
2026-03-17 15:22:47 +0800

    文件存储与下载

--- start/src/main/java/cn/edu/qcl/Application.java
+++ start/src/main/java/cn/edu/qcl/Application.java
@@ -2,12 +2,14 @@ package cn.edu.qcl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /**
  * Spring Boot Starter
  *
  */
 @SpringBootApplication(scanBasePackages = {"cn.edu.qcl"})
+@ConfigurationPropertiesScan(basePackages = {"cn.edu.qcl.config"})
 //@MapperScan({"cn.edu.qcl"})
 public class Application {
 
